80e99d4ffca87f261819881bd8b3459f14429723
Ravi Kumar <rsravi.k9@gmail.com>
2023-08-08 17:22:42 +0530

    Changes

--- src/components/Board/constant.js
+++ src/components/Board/constant.js
@@ -20,7 +20,26 @@ export const mapData = [
                 labels: [{ text: 'BackEnd', color: 'brown' }],
                 description: 'This is BackEnd Description',
                 date: ''
-            }
+            },
+
+            {
+                id: Date.now() + Math.random(),
+                title: 'Card 2',
+                tasks: [],
+                labels: [{ text: 'FrontEnd', color: 'blue' }],
+                description: 'This is FrontEnd Description',
+                date: ''
+            },
+
+            {
+                id: Date.now() + Math.random(),
+                title: 'Card 3',
+                tasks: [],
+                labels: [{ text: 'BackEnd', color: 'brown' }],
+                description: 'This is BackEnd Description',
+                date: ''
+            },
+
 
         ]
     }
